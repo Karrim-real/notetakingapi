5b1587edc26970f43e2263a1c76bbea1b944e163
Akinyemi Taiwo <karrim4real@gmail.com>
2022-09-19 18:03:00 -0400

    some file added

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\BookController;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,5 +20,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiResource('book', BookController::class);
+
+Route::apiResource('book', BookController::class)->middleware(['cors']);
 
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/foo', function () {
+    Artisan::call('storage:link');
+    return 'foo page';
+});
+
+
+
